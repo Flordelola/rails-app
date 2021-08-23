44514f841833841f88ef1e14f9f9a26d5bd4f2ba
flordelola <mayra@cool-tabs.com>
2021-08-23 12:59:36 +0100

    Fix call twitter account username

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -7,6 +7,6 @@ class TwitterAccountsController < ApplicationController
   def destroy
     @twitter_account = Current.user.twitter_accounts.find(params[:id])
     @twitter_account.destroy
-    redirect_to twitter_accounts_path, notice: "Successfully disconnected @#{twitter_account.username}"
+    redirect_to twitter_accounts_path, notice: "Successfully disconnected @#{@twitter_account.username}"
   end
 end
